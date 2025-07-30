7d5644f731bbb6d0e31b8e323cd68ebceb4e7de0
Aidan Hong <141174996+crimmyy@users.noreply.github.com>
2025-07-30 09:26:08 -0400

    Update Airtable.tsx

--- src/components/Airtable.tsx
+++ src/components/Airtable.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 const Airtable: React.FC = () => {
   return (
-    <div className="w-full h-screen md:h-screen rounded-[16px] overflow-hidden border-[2px] border-[#002370]">
+    <div className="w-full rounded-[16px] overflow-hidden border-[2px] border-[#002370] bg-[#EBF5FE]">
       <iframe
-        className="w-full h-full"
+        className="w-full min-h-[1000px] md:min-h-[1200px]"
         src="https://airtable.com/embed/app5njLO852rBMkWj/pagr30Zr2DdZu8kXA/form"
         frameBorder="0"
         style={{ background: 'transparent' }}
         title="Airtable Form"
+        scrolling="yes"
       ></iframe>
     </div>
   );
